c74d1cd45875ab27e93d847f2cc7b30dfad2a8d7
AJ Ancheta <ashleyjancheta@gmail.com>
2019-07-26 12:47:04 -0700

    add placeholder text to test if navbar is fixed // splash in progress

---
+++ src/components/Splash.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function Splash() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Splash works</h1>
+      <p>Lorem in nulla sit ipsum duis. Officia laboris pariatur elit duis ipsum elit eiusmod proident quis cillum sunt excepteur. Ex ex ex tempor cillum quis esse deserunt ipsum eiusmod veniam. In aute adipisicing excepteur eu laborum exercitation nulla occaecat dolore ut fugiat non. Sit sint nisi aute quis. Est laboris consectetur id esse aute minim eiusmod anim amet laboris minim ex consectetur exercitation.
+
+Tempor mollit esse labore laboris do enim duis eiusmod labore culpa veniam aliqua minim. Dolor enim in culpa est labore pariatur occaecat ullamco id aliqua ullamco. Eu duis esse aliqua laboris amet occaecat irure amet excepteur mollit. Non consequat sit adipisicing commodo.
+
+Est ut ad in cupidatat fugiat est elit quis culpa mollit aute consequat ex mollit. Incididunt consequat minim Lorem eiusmod id consequat eiusmod tempor anim dolor nulla enim ut. Id elit laborum reprehenderit occaecat irure commodo id aliquip nulla amet magna officia enim nulla. Eiusmod veniam occaecat irure officia fugiat commodo incididunt sunt sint reprehenderit dolore nostrud. Duis ex dolor enim eiusmod Lorem do ullamco ut ea dolore fugiat adipisicing. Ullamco aliquip laboris labore nostrud in tempor exercitation id eiusmod ex magna deserunt velit aute. Aliqua et fugiat velit magna duis est pariatur aute incididunt.
+
+Id et cillum deserunt nisi ad velit. Deserunt dolore amet magna laboris nostrud tempor esse. Sit nostrud tempor do velit qui dolore anim proident non culpa laborum. Sit magna sunt labore ullamco do duis incididunt velit proident deserunt.
+
+Lorem in nulla sit ipsum duis. Officia laboris pariatur elit duis ipsum elit eiusmod proident quis cillum sunt excepteur. Ex ex ex tempor cillum quis esse deserunt ipsum eiusmod veniam. In aute adipisicing excepteur eu laborum exercitation nulla occaecat dolore ut fugiat non. Sit sint nisi aute quis. Est laboris consectetur id esse aute minim eiusmod anim amet laboris minim ex consectetur exercitation.
+
+Tempor mollit esse labore laboris do enim duis eiusmod labore culpa veniam aliqua minim. Dolor enim in culpa est labore pariatur occaecat ullamco id aliqua ullamco. Eu duis esse aliqua laboris amet occaecat irure amet excepteur mollit. Non consequat sit adipisicing commodo.
+
+Est ut ad in cupidatat fugiat est elit quis culpa mollit aute consequat ex mollit. Incididunt consequat minim Lorem eiusmod id consequat eiusmod tempor anim dolor nulla enim ut. Id elit laborum reprehenderit occaecat irure commodo id aliquip nulla amet magna officia enim nulla. Eiusmod veniam occaecat irure officia fugiat commodo incididunt sunt sint reprehenderit dolore nostrud. Duis ex dolor enim eiusmod Lorem do ullamco ut ea dolore fugiat adipisicing. Ullamco aliquip laboris labore nostrud in tempor exercitation id eiusmod ex magna deserunt velit aute. Aliqua et fugiat velit magna duis est pariatur aute incididunt.
+
+Id et cillum deserunt nisi ad velit. Deserunt dolore amet magna laboris nostrud tempor esse. Sit nostrud tempor do velit qui dolore anim proident non culpa laborum. Sit magna sunt labore ullamco do duis incididunt velit proident deserunt.
+
+Lorem in nulla sit ipsum duis. Officia laboris pariatur elit duis ipsum elit eiusmod proident quis cillum sunt excepteur. Ex ex ex tempor cillum quis esse deserunt ipsum eiusmod veniam. In aute adipisicing excepteur eu laborum exercitation nulla occaecat dolore ut fugiat non. Sit sint nisi aute quis. Est laboris consectetur id esse aute minim eiusmod anim amet laboris minim ex consectetur exercitation.
+
+Tempor mollit esse labore laboris do enim duis eiusmod labore culpa veniam aliqua minim. Dolor enim in culpa est labore pariatur occaecat ullamco id aliqua ullamco. Eu duis esse aliqua laboris amet occaecat irure amet excepteur mollit. Non consequat sit adipisicing commodo.
+
+Est ut ad in cupidatat fugiat est elit quis culpa mollit aute consequat ex mollit. Incididunt consequat minim Lorem eiusmod id consequat eiusmod tempor anim dolor nulla enim ut. Id elit laborum reprehenderit occaecat irure commodo id aliquip nulla amet magna officia enim nulla. Eiusmod veniam occaecat irure officia fugiat commodo incididunt sunt sint reprehenderit dolore nostrud. Duis ex dolor enim eiusmod Lorem do ullamco ut ea dolore fugiat adipisicing. Ullamco aliquip laboris labore nostrud in tempor exercitation id eiusmod ex magna deserunt velit aute. Aliqua et fugiat velit magna duis est pariatur aute incididunt.
+
+Id et cillum deserunt nisi ad velit. Deserunt dolore amet magna laboris nostrud tempor esse. Sit nostrud tempor do velit qui dolore anim proident non culpa laborum. Sit magna sunt labore ullamco do duis incididunt velit proident deserunt.
+
+Lorem in nulla sit ipsum duis. Officia laboris pariatur elit duis ipsum elit eiusmod proident quis cillum sunt excepteur. Ex ex ex tempor cillum quis esse deserunt ipsum eiusmod veniam. In aute adipisicing excepteur eu laborum exercitation nulla occaecat dolore ut fugiat non. Sit sint nisi aute quis. Est laboris consectetur id esse aute minim eiusmod anim amet laboris minim ex consectetur exercitation.
+
+Tempor mollit esse labore laboris do enim duis eiusmod labore culpa veniam aliqua minim. Dolor enim in culpa est labore pariatur occaecat ullamco id aliqua ullamco. Eu duis esse aliqua laboris amet occaecat irure amet excepteur mollit. Non consequat sit adipisicing commodo.
+
+Est ut ad in cupidatat fugiat est elit quis culpa mollit aute consequat ex mollit. Incididunt consequat minim Lorem eiusmod id consequat eiusmod tempor anim dolor nulla enim ut. Id elit laborum reprehenderit occaecat irure commodo id aliquip nulla amet magna officia enim nulla. Eiusmod veniam occaecat irure officia fugiat commodo incididunt sunt sint reprehenderit dolore nostrud. Duis ex dolor enim eiusmod Lorem do ullamco ut ea dolore fugiat adipisicing. Ullamco aliquip laboris labore nostrud in tempor exercitation id eiusmod ex magna deserunt velit aute. Aliqua et fugiat velit magna duis est pariatur aute incididunt.
+
+Id et cillum deserunt nisi ad velit. Deserunt dolore amet magna laboris nostrud tempor esse. Sit nostrud tempor do velit qui dolore anim proident non culpa laborum. Sit magna sunt labore ullamco do duis incididunt velit proident deserunt.</p>
+
+    </div>
+  );
+}
+
+export default Splash;

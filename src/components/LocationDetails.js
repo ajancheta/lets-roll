c07dc494252e8a8840c974954af682c5ad0ac04e
AJ Ancheta <ashleyjancheta@gmail.com>
2019-08-01 07:58:27 -0700

    edit structure of details

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -3,20 +3,27 @@ import PropTypes from 'prop-types';
 import '../css/LocationDetails.css';
 import Navbar from './Navbar';
 import Header from '../assets/img/details.png';
+import Pin from '../assets/img/map-pin.png';
 
 function LocationDetails(props) {
   return (
     
-    <div className="location-container">
+    <div className="details-container">
       <Navbar />
       <div className="explore-photo">
         <img src={Header} />
       </div>
-      <div className="location-details-container">
-        <div className="location-details-text">
-          <h3>Nemesis Jiu Jitsu</h3>
-          <h4>635 NW 16th Ave, Portland, OR 97209</h4>
-          <p><span className="open-mat">Open Mat:</span> Sunday <span className="divider">| </span>10 AM to 11 AM</p>
+      <div className="details-text-container">
+        <div className="details-text">
+          <h3><span className="open-mat">Open mat:</span> Sunday <span className="divider">| </span>10 AM to 11 AM</h3>
+
+          <div className="address-detail">
+            <span><img src={Pin} /></span><h4>635 NW 16th Ave, Portland, OR 97209</h4>
+          </div>
+        </div>
+
+        <div className="map-container">
+          
         </div>
       </div>
     </div>

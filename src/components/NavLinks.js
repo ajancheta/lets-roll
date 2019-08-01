c55df796bbd04102676bf7a9da30e7167f624549
AJ Ancheta <ashleyjancheta@gmail.com>
2019-08-01 03:30:18 -0700

    add sidebar links and style

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../css/NavLinks.css';
+import { Link } from 'react-router-dom';
+import About from './About';
 
 function NavLinks() {
   return (
     <div className="nav-links-container">
-      <li>Home</li>
-      <li>About</li>
+      <Link to="/" style={{ textDecoration: 'none'}}><li>Home</li></Link>
+      <Link to="/about" style={{ textDecoration: 'none'}}><li>About</li></Link>
       <li>Getting Started</li>
       <li>All Locations</li>
     </div>

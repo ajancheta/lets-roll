91034000e279231e9c2f008a65cbf74cca305a38
AJ Ancheta <ashleyjancheta@gmail.com>
2019-07-31 20:34:37 -0700

    install firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+//initialize firebase
+
+const config = {
+  apiKey: "AIzaSyAA7YyUh_S_y_MzDxqUWvqWsvkXK7H6egA",
+  authDomain: "let-s-roll-aff0b.firebaseapp.com",
+  databaseURL: "https://let-s-roll-aff0b.firebaseio.com",
+  projectId: "let-s-roll-aff0b",
+  storageBucket: "",
+  messagingSenderId: "723299742056",
+  appId: "1:723299742056:web:369c5df6dd7ae1b6"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true})
+
+export default firebase;

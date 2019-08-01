be10afd5cf940b0a116ff57de21892b817d1715e
AJ Ancheta <ashleyjancheta@gmail.com>
2019-08-01 03:31:06 -0700

    create custom database of open mat info and render to display

---
+++ src/components/Location.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../css/Location.css';
+import { Link } from 'react-router-dom';
+
+function Location(props) {
+  return (
+    
+    <div className="location-container">
+      <div className="current-locations-text-container">
+        <div className="location-details-container">
+        <Link to="/" style={{textDecoration: 'none'}}>
+          <div className="location-details">
+            <h3>{props.name}</h3>
+            <h4>{props.address}</h4>
+            <p><span className="open-mat">Open Mat:</span> {props.day} <span className="divider">| </span>{props.hours}</p>
+          </div>
+        </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+Location.propTypes = {
+  name: PropTypes.string,
+  address: PropTypes.string,
+  hours: PropTypes.string,
+  day: PropTypes.string,
+  website: PropTypes.string,
+  IG: PropTypes.string,
+};
+
+export default Location;
---
+++ src/components/LocationList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../css/LocationList.css';
+import Navbar from './Navbar';
+import Location from './Location';
+import BJJPhoto from '../assets/img/explore.png';
+
+function LocationList(props) {
+  return (
+    <div className="location-list-container">
+      <Navbar />
+      <div className="explore-photo">
+        <img src={BJJPhoto} />
+      </div>
+
+      <div className="locations-text-container">
+        {props.locationList.map(location => (
+          <Location
+            name={location.name}
+            address={location.address}
+            day={location.day}
+            hours={location.hours}
+            website={location.website}
+            IG={location.IG}
+            key={location.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+LocationList.propTypes = {
+  locationList: PropTypes.array
+};
+
+export default LocationList;
